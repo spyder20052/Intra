fb3b7562d949fd88c13a314b1890f5e910f0d33c
Spyder20052 <spynel.kouton@epitech.eu>
2025-11-25 14:45:43 +0100

    Add V2.02

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
 module.exports = {
 	darkMode: ['class'],
 	content: [
@@ -16,6 +17,9 @@ module.exports = {
 			},
 		},
 		extend: {
+			fontFamily: {
+				sans: ['Fredoka', ...fontFamily.sans],
+			},
 			colors: {
 				border: 'hsl(var(--border))',
 				input: 'hsl(var(--input))',
